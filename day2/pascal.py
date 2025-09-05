cb0e492d789d68e9b60417d024eb88bc43bd5524
[200~AnveshAnnepaga~ <annepagaanvesh44@gmail.com>
2025-09-05 18:33:28 +0530

    update with Day 2 progress on Python programming

--- day2/pascal.py
+++ day2/pascal.py
@@ -1,3 +1,16 @@
-def pasc(x):
-    for i in range(1,x):
-        for j in range(1,i+1):
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    
+    for i in range(n):
+        print(" " * (n - i), end="")   # spacing for alignment
+        for num in triangle[i]:
+            print(num, end=" ")
+        print()
+
+pascal_triangle(5)
